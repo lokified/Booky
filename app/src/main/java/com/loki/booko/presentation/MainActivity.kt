41c19c59422c03846569554e7792a65a010497f9
Sheldon Okware <sheldon.okware@lampnets.com>
2023-01-16 08:43:13 +0300

    Add a splash screen for both themes

--- app/src/main/java/com/loki/booko/presentation/MainActivity.kt
+++ app/src/main/java/com/loki/booko/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.loki.booko.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -11,16 +12,28 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.loki.booko.presentation.components.BottomNav
+import com.loki.booko.presentation.home.HomeViewModel
 import com.loki.booko.presentation.navigation.Navigation
 import com.loki.booko.presentation.theme.BookoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: HomeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.bookState.value.isLoading
+            }
+        }
+
         setContent {
             BookoTheme {
 
